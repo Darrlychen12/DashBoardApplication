d520155bb041ed1aa366e23efc9be2afbcbb7d01
Darryl <chendarryl97@gmail.com>
2023-03-02 10:52:41 -0800

    feat: added additional setup/boilerplate code

--- server/index.js
+++ server/index.js
@@ -4,6 +4,11 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import clientRoutes from "./routes/client.js";
+import generalRoutes from "./routes/general.js";
+import managementRoutes from "./routes/management.js";
+import salesRoutes from "./routes/sales.js";
+
 
 /*CONFIGURATION*/
 dotenv.config;
@@ -14,4 +19,10 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin"}));
 app.use(morgan("common"));
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false}));
-app.use(cors())
+app.use(cors());
+
+
+app.use("/client", clientRoutes);
+app.use("/general", generalRoutes);
+app.use("/management", managementRoutes);
+app.use("/sales", salesRoutes);
