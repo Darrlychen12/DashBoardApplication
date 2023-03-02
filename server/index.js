77291ef094b08e84e99f629143e79ca83e7f6b1b
Darryl <chendarryl97@gmail.com>
2023-03-02 13:17:23 -0800

    feat: added additional boilerplate and theme file

--- server/index.js
+++ server/index.js
@@ -8,10 +8,11 @@ import clientRoutes from "./routes/client.js";
 import generalRoutes from "./routes/general.js";
 import managementRoutes from "./routes/management.js";
 import salesRoutes from "./routes/sales.js";
+import mongoose from 'mongoose';
 
 
 /*CONFIGURATION*/
-dotenv.config;
+dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
@@ -21,8 +22,21 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(cors());
 
-
+/*ROUTES*/
 app.use("/client", clientRoutes);
 app.use("/general", generalRoutes);
 app.use("/management", managementRoutes);
 app.use("/sales", salesRoutes);
+console.log(process.env.MONGO_URL);
+
+/*MONGOOSE SETUP*/
+const PORT = process.env.PORT || 9000;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
